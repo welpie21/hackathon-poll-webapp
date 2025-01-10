4a5a1790c53b4c88f4723483d3f6430bffee8d3a
ekwuno <Obinnacodes@gmail.com>
2025-01-10 13:04:35 +0000

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import type { Component } from 'solid-js';
+import { Router, Routes, Route } from "@solidjs/router";
 import { SurrealProvider } from './lib/providers/surrealdb';
 import { QueryClientProvider } from '@tanstack/solid-query';
 import { tanstackClient } from './lib/query-client';
 import { Navbar } from './components/Navbar';
 import { AuthProvider } from './lib/providers/auth';
+import Home from './pages/Home';
+import CreatePoll from './pages/CreatePoll';
+import VotePoll from './pages/VotePoll';
+import PollResults from './pages/PollResults';
 
 const App: Component = () => {
 	return (
@@ -15,6 +20,15 @@ const App: Component = () => {
 				<AuthProvider>
 					<Navbar />
 				</AuthProvider>
+				<Navbar />
+				<Router>
+					<Routes>
+						<Route path="/" component={Home} />
+						<Route path="/create" component={CreatePoll} />
+						<Route path="/poll/:id" component={VotePoll} />
+						<Route path="/poll/:id/results" component={PollResults} />
+					</Routes>
+				</Router>
 			</SurrealProvider>
 		</QueryClientProvider>
 	);
