0d71c74b5e75aafeeda2efa587ba0d1a80fe4b68
Beau den Heijer <beaudenheijer@gmail.com>
2025-01-10 11:10:44 +0100

    Added tailwind and setup project progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
 import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { SurrealProvider } from './lib/providers/surrealdb';
+import { QueryClientProvider } from '@tanstack/solid-query';
+import { tanstackClient } from './lib/query-client';
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<QueryClientProvider client={tanstackClient}>
+			<SurrealProvider 
+				endpoint='http://127.0.0.1:8000' 
+				autoConnect
+			>
+				<div />
+			</SurrealProvider>
+		</QueryClientProvider>
+	);
 };
 
 export default App;
