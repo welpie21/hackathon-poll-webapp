95b610677147ddaab29c1e861665b6a5cd2a6f9f
Beau den Heijer <beaudenheijer@gmail.com>
2025-01-10 11:41:26 +0100

    Added aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { resolve } from "node:path";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+	plugins: [solidPlugin()],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: 'esnext',
+	},
+	resolve: {
+		alias: {
+			"~": resolve(__dirname, "./src")
+		}
+	}
 });
